9cc3a2f86f050676f921098f7dcd05fd8053c230
M1roel <ppfautsch@yahoo.de>
2024-10-15 21:15:45 +0200

    random spawns for enemies

--- src/models/fish.class.js
+++ src/models/fish.class.js
@@ -2,5 +2,10 @@ class Fish extends MoveableObject {
 
     constructor() {
         super().loadImg('/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png')
+
+        this.x = 250 + Math.random() * 400;
+        this.y = 250 + Math.random() * 100;
+        this.height = 100;
+        this.width = 100;
     }
 }
