130fc4bbeb07a48887a6dd13c42349f074b1579c
M1roel <ppfautsch@yahoo.de>
2024-10-15 19:46:19 +0200

    add new class.js and DA Checklist for the project

--- src/js/game.js
+++ src/js/game.js
@@ -1,10 +1,10 @@
 let canvas;
 let ctx;
-let charakter = new Image();
+let charakter = new MoveableObject();
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    charakter.src = '/public/img/1.Sharkie/1.IDLE/1.png';
-    ctx.drawImage(charakter, 20, 20, 50, 150);
+    
+    console.log('My charakter is', MoveableObject);
 }
---
+++ src/models/moveable-object.class.js
@@ -0,0 +1,9 @@
+class MoveableObject {
+    x = 400;
+    y = 200;
+    img;
+
+    moveRight() {
+        console.log('Moving right');
+    }
+}
