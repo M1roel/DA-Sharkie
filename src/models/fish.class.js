79c412d61fce6d97b108bdc72964bae293b4de75
M1roel <ppfautsch@yahoo.de>
2024-10-16 19:51:43 +0200

    add amimation for enemies

--- src/models/fish.class.js
+++ src/models/fish.class.js
@@ -4,8 +4,15 @@ class Fish extends MoveableObject {
         super().loadImg('/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
 
         this.x = 250 + Math.random() * 400;
-        this.y = 250 + Math.random() * 100;
+        this.y = 20 + Math.random() * 400;
         this.height = 100;
         this.width = 100;
+        this.animate();
+    }
+    
+    animate() {
+        setInterval(() => {
+            this.x -= 0.15;
+        }, 1000 / 60);
     }
 }
