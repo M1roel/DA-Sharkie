afaa4a0da8b46f8ed026fce7c27ea5ade8730e61
M1roel <ppfautsch@yahoo.de>
2024-10-20 13:53:42 +0200

    create endboss.class and import images

---
+++ src/models/endboss.class.js
@@ -0,0 +1,31 @@
+class Endboss extends MoveableObject {
+
+    IMAGES_INTRODUCE = [
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/1.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/2.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/3.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/4.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/5.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/6.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/7.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/8.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/9.png",
+        "/public/img/2.Enemy/3 Final Enemy/1.Introduce/10.png"
+    ];
+
+    IMAGES_FLOATING = [
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/1.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/2.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/3.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/4.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/5.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/6.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/7.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/8.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/9.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/10.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/11.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/12.png",
+        "/public/img/2.Enemy/3 Final Enemy/2.floating/13.png"
+    ];
+}
