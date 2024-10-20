d86c7f255b8475cc1f3aee32b426531e2b3cd8ae
M1roel <ppfautsch@yahoo.de>
2024-10-20 19:29:24 +0200

    optimize the appearance of the endboss

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -2,15 +2,8 @@ const backgroundObjects = [];
 const backgroundWidth = 719;
 
 for (let i = 0; i < 8; i++) {
-  const img = i % 2 === 0 
-    ? "/public/img/3. Background/Dark/2.png" 
-    : "/public/img/3. Background/Dark/1.png";
+  const img = i % 2 === 0 ? "/public/img/3. Background/Dark/2.png" : "/public/img/3. Background/Dark/1.png";
   backgroundObjects.push(new BackgroundObject(img, i * backgroundWidth));
 }
 
-const level1 = new Level(
-  backgroundObjects,
-  [new Fish(), new Fish(), new Fish()],
-  [new Light()],
-  new Endboss()
-);
+const level1 = new Level(backgroundObjects, [new Fish(), new Fish(), new Fish()], [new Light()], new Endboss());
