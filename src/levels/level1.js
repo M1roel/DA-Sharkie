6023d78f890120e17efdcdcd8c01c2c53eeb3599
M1roel <ppfautsch@yahoo.de>
2024-10-24 21:31:00 +0200

    try to generate random color fishes

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -1,9 +1,21 @@
 const backgroundObjects = [];
 const backgroundWidth = 719;
+const fishCount = 8;
 
 for (let i = 0; i < 8; i++) {
   const img = i % 2 === 0 ? "/public/img/3. Background/Dark/2.png" : "/public/img/3. Background/Dark/1.png";
   backgroundObjects.push(new BackgroundObject(img, i * backgroundWidth));
 }
 
-const level1 = new Level(backgroundObjects, [new Fish(), new Fish(), new Fish()], [new Light()], new Endboss());
+function createRandomFish() {
+  const colors = ['GREEN', 'RED', 'VIOLET'];
+  const randomColor = colors[Math.floor(Math.random() * colors.length)];
+  return new Fish(randomColor);
+}
+
+const fishArray = [];
+for (let i = 0; i < fishCount; i++) {
+  fishArray.push(createRandomFish());
+}
+
+const level1 = new Level(backgroundObjects, fishArray, [new Light()], new Endboss());
--- src/models/fish.class.js
+++ src/models/fish.class.js
@@ -1,17 +1,36 @@
 class Fish extends MoveableObject {
+  IMAGES_GREEN = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png"];
+  IMAGES_RED = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim5.png"];
+  IMAGES_VIOLET = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim5.png"];
 
-    constructor() {
-        super().loadImg('/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
+  constructor(color) {
+    super();
 
-        this.x = 250 + Math.random() * 400;
-        this.y = 20 + Math.random() * 400;
-        this.height = 100;
-        this.width = 100;
-        this.animate();
-        this.speed = 0.15 + Math.random() * 0.5
+    this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png");
+    this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png");
+    this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png");
+
+    this.loadImgs(this.IMAGES_GREEN);
+    this.loadImgs(this.IMAGES_RED);
+    this.loadImgs(this.IMAGES_VIOLET);
+
+    if (color === "GREEN") {
+      this.loadImgs(this.IMAGES_GREEN);
+    } else if (color === "RED") {
+      this.loadImgs(this.IMAGES_RED);
+    } else if (color === "VIOLET") {
+      this.loadImgs(this.IMAGES_VIOLET);
     }
-    
-    animate() {
-        this.moveLeft();
-    }
+
+    this.x = 250 + Math.random() * 400;
+    this.y = 20 + Math.random() * 400;
+    this.height = 100;
+    this.width = 100;
+    this.animate();
+    this.speed = 0.15 + Math.random() * 0.5;
+  }
+
+  animate() {
+    this.moveLeft();
+  }
 }
