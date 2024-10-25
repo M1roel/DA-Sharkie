3d4b5bcd62aaa44509f1dbbaf083963a82b4f9cc
M1roel <ppfautsch@yahoo.de>
2024-10-25 09:24:40 +0200

    optimize the generate random color fish function

--- src/models/fish.class.js
+++ src/models/fish.class.js
@@ -1,24 +1,19 @@
 class Fish extends MoveableObject {
   IMAGES_GREEN = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png"];
-  IMAGES_RED = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim5.png"];
+  IMAGES_RED = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim5.png"];
   IMAGES_VIOLET = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim5.png"];
 
   constructor(color) {
     super();
 
-    this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png");
-    this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png");
-    this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png");
-
-    this.loadImgs(this.IMAGES_GREEN);
-    this.loadImgs(this.IMAGES_RED);
-    this.loadImgs(this.IMAGES_VIOLET);
-
     if (color === "GREEN") {
+      this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png");
       this.loadImgs(this.IMAGES_GREEN);
     } else if (color === "RED") {
+      this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png");
       this.loadImgs(this.IMAGES_RED);
     } else if (color === "VIOLET") {
+      this.loadImg("/public/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png");
       this.loadImgs(this.IMAGES_VIOLET);
     }
 
