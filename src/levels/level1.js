6d516da9ce5207717310e78c7157ee779eb3766e
M1roel <ppfautsch@yahoo.de>
2024-10-25 09:29:21 +0200

    generate an extra image for left level side, to get no black screen

--- src/levels/level1.js
+++ src/levels/level1.js
@@ -2,6 +2,9 @@ const backgroundObjects = [];
 const backgroundWidth = 719;
 const fishCount = 8;
 
+
+backgroundObjects.push(new BackgroundObject("/public/img/3. Background/Dark/1.png", -719));
+
 for (let i = 0; i < 8; i++) {
   const img = i % 2 === 0 ? "/public/img/3. Background/Dark/2.png" : "/public/img/3. Background/Dark/1.png";
   backgroundObjects.push(new BackgroundObject(img, i * backgroundWidth));
