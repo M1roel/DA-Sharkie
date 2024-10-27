8e56e18766a0f67f1c791d448586acf2ca2196e3
M1roel <ppfautsch@yahoo.de>
2024-10-27 16:05:49 +0100

    try to implement enrage modus for enemies

--- src/models/fish.class.js
+++ src/models/fish.class.js
@@ -9,6 +9,8 @@ class Fish extends MoveableObject {
   IMAGES_RED_BUBBLESWIN = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/2.bubbleswim5.png"];
   IMAGES_VIOLET_BUBBLESWIN = ["/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim1.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim2.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim3.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim4.png", "/public/img/2.Enemy/1.Puffer fish (3 color options)/3.Bubbleeswim/3.bubbleswim5.png"];
   
+  currentImage = 0;
+
   constructor(color) {
     super();
 
@@ -23,23 +25,36 @@ class Fish extends MoveableObject {
       this.loadImg(this.IMAGES_GREEN[0]);
       this.currentImages = this.IMAGES_GREEN;
       this.enrageImages = this.IMAGES_GREEN_ENRAGE;
+      this.bubbleswimImages = this.IMAGES_GREEN_BUBBLESWIN;
     } else if (color === "RED") {      
       this.loadImg(this.IMAGES_RED[0]);
       this.currentImages = this.IMAGES_RED;
       this.enrageImages = this.IMAGES_RED_ENRAGE;
+      this.bubbleswimImages = this.IMAGES_RED_BUBBLESWIN;
     } else if (color === "VIOLET") {
       this.loadImg(this.IMAGES_VIOLET[0]);
       this.currentImages = this.IMAGES_VIOLET;
       this.enrageImages = this.IMAGES_VIOLET_ENRAGE;
+      this.bubbleswimImages = this.IMAGES_VIOLET_BUBBLESWIN;
     }
     this.loadImgs(this.currentImages);
+    this.loadImgs(this.enrageImages);
+    this.loadImgs(this.bubbleswimImages);
     this.animate();
+  }
+  
+  animate() {
+    this.moveLeft();
   }
 
   getEnrage() {
     if (!this.enrageActive) {
-      this.enrageActive = true; 
-      this.loadImgs(this.enrageImages);
+      this.enrageActive = true;
+      try {        
+      this.animateEnrage(this.enrageImages);
+      } catch (error) {
+        console.log('Fehler beim laden der Bilder');
+      } 
       this.speed *= 1.5;
       console.log("Fish is enraged!");
     }
@@ -54,9 +69,13 @@ class Fish extends MoveableObject {
     }
   }
 
-  animate() {
-    this.moveLeft();
-    this.getEnrage();
-    this.resetEnrage();
+  animateEnrage(array) {
+    setInterval(() => {
+      
+    }, interval);
+    let i = tis.currentImage % array.length;
+      let path = array[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
   }
 }
