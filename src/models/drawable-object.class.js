d752b9cbde4a475490df03cadea0258fe25282d9
M1roel <ppfautsch@yahoo.de>
2024-10-29 16:47:18 +0100

    add two new model files and cut off several variables from moveableobject class to drawableobject class

---
+++ src/models/drawable-object.class.js
@@ -0,0 +1,12 @@
+class DrawableObject {
+    x = 50;
+    y = 300;
+    height = 200;
+    width = 200;
+    hitboxWidth = 0;
+    hitboxHeight = 0;
+    enrageWidth = 100;
+    enrageHeight = 0;
+    img;
+    imageCache = {};
+}
--- src/models/moveable-object.class.js
+++ src/models/moveable-object.class.js
@@ -1,14 +1,5 @@
-class MoveableObject {
-  x = 50;
-  y = 300;
-  height = 200;
-  width = 200;
-  hitboxWidth = 0;
-  hitboxHeight = 0;
-  enrageWidth = 100;
-  enrageHeight = 0;
-  img;
-  imageCache = {};
+class MoveableObject extends DrawableObject {
+ 
   otherDirection = false;
   deathAnimationFinished = false;
   energy = 100;
