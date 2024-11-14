46f68e102f088ccb65fabd8aaa3e2501f0a6915b
M1roel <ppfautsch@yahoo.de>
2024-11-14 20:12:22 +0100

    try to implement bubble hit for endboss

--- src/models/endboss.class.js
+++ src/models/endboss.class.js
@@ -5,16 +5,17 @@ class Endboss extends MoveableObject {
   height = 400;
 
   IMAGES_INTRODUCE = ["/public/img/2.Enemy/3 Final Enemy/1.Introduce/1.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/2.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/3.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/4.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/5.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/6.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/7.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/8.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/9.png", "/public/img/2.Enemy/3 Final Enemy/1.Introduce/10.png"];
-  IMAGES_ATTACK = ["/public/img/2.Enemy/3 Final Enemy/Attack/1.png", "/public/img/2.Enemy/3 Final Enemy/Attack/2.png", "/public/img/2.Enemy/3 Final Enemy/Attack/3.png", "/public/img/2.Enemy/3 Final Enemy/Attack/4.png", "/public/img/2.Enemy/3 Final Enemy/Attack/5.png", "/public/img/2.Enemy/3 Final Enemy/Attack/6.png"]
+  IMAGES_ATTACK = ["/public/img/2.Enemy/3 Final Enemy/Attack/1.png", "/public/img/2.Enemy/3 Final Enemy/Attack/2.png", "/public/img/2.Enemy/3 Final Enemy/Attack/3.png", "/public/img/2.Enemy/3 Final Enemy/Attack/4.png", "/public/img/2.Enemy/3 Final Enemy/Attack/5.png", "/public/img/2.Enemy/3 Final Enemy/Attack/6.png"];
   IMAGES_FLOATING = ["/public/img/2.Enemy/3 Final Enemy/2.floating/1.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/2.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/3.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/4.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/5.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/6.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/7.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/8.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/9.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/10.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/11.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/12.png", "/public/img/2.Enemy/3 Final Enemy/2.floating/13.png"];
   IMAGES_HURT = ["/public/img/2.Enemy/3 Final Enemy/Hurt/1.png", "/public/img/2.Enemy/3 Final Enemy/Hurt/2.png", "/public/img/2.Enemy/3 Final Enemy/Hurt/3.png", "/public/img/2.Enemy/3 Final Enemy/Hurt/4.png"];
   IMAGES_DEAD = ["/public/img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 6.png", "/public/img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 7.png", "/public/img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 8.png", "/public/img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 9.png", "/public/img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2 copia 10.png", "/public/img/2.Enemy/3 Final Enemy/Dead/Mesa de trabajo 2.png"];
 
   world;
-  endboss_sound = new Audio('/src/audio/endboss.mp3');
+  endboss_sound = new Audio("/src/audio/endboss.mp3");
   hitByBubble = false;
   isDead = false;
   currentImage = 0;
+  lifes = 5;
 
   constructor() {
     super().loadImg("/public/img/2.Enemy/3 Final Enemy/1.Introduce/1.png");
@@ -62,5 +63,44 @@ class Endboss extends MoveableObject {
       this.img = this.imageCache[path];
       this.currentImage++;
     }, 1000 / 5);
+  }
+
+  handleBubbleHit() {
+    // Überprüfen, ob der Endboss bereits getroffen oder tot ist
+    if (this.isDead || this.hitByBubble) return;
+
+    this.hitByBubble = true;
+    this.lifes--;
+
+    // Überprüfen, ob der Endboss besiegt wurde
+    if (this.lifes <= 0) {
+      this.isDead = true;
+      this.playDeathAnimation();
+    } else {
+      this.playHurtAnimation();
+      // Nach kurzer Zeit zurücksetzen, damit weitere Treffer möglich sind
+      setTimeout(() => {
+        this.hitByBubble = false;
+      }, 500);
+    }
+  }
+
+  // Animationen für den verletzten Endboss
+  playHurtAnimation() {
+    this.loadAnimation("IMAGES_HURT");
+    // Wählen Sie hier, wie lange die Verletzungsanimation laufen soll
+    setTimeout(() => {
+      this.currentImage = 0;
+      this.loadAnimation(this.IMAGES_FLOATING); // Zurück zur Standardanimation
+    }, 1000); // Beispiel: 1 Sekunde
+  }
+
+  // Animation für das Ableben des Endbosses
+  playDeathAnimation() {
+    this.loadAnimation("IMAGES_DEAD");
+    setTimeout(() => {
+      // Weitere Aktionen, die nach dem Tod des Endbosses ablaufen sollen, wie z.B. das Entfernen aus dem Spiel
+      this.flyOff();
+    }, 2000); // Dauer der Todesanimation
   }
 }
