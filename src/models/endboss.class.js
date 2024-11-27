2648edcc73b199838a15eae80d72cfb824fe14f8
M1roel <ppfautsch@yahoo.de>
2024-11-27 18:56:19 +0100

    bugfixed shotBubble() function

--- src/models/endboss.class.js
+++ src/models/endboss.class.js
@@ -28,10 +28,10 @@ class Endboss extends MoveableObject {
     this.loadImgs(this.IMAGES_HURT);
     this.loadImgs(this.IMAGES_DEAD);
     this.animate();
-    this.hitboxX = 250;
-    this.hitboxY = 250;
-    this.hitboxWidth = 300;
-    this.hitboxHeight = 300;
+    this.hitboxX = 80;
+    this.hitboxY = 80;
+    this.hitboxWidth = 250;
+    this.hitboxHeight = 250;
     this.width = 300;
     this.height = 300;
   }
