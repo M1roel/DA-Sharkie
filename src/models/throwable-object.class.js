6d6091b8238199ecdf9307cb34ba24e797ac86b6
M1roel <ppfautsch@yahoo.de>
2024-11-30 22:43:05 +0100

    JSDocs comments for throwable-object class

--- src/models/throwable-object.class.js
+++ src/models/throwable-object.class.js
@@ -1,5 +1,13 @@
 class ThrowableObject extends MoveableObject {
 
+    /**
+     * Creates a new throwable object with a specified position and type.
+     * The object can either be a "normal" bubble or a "poison" bubble.
+     * 
+     * @param {number} x - The x-coordinate of the throwable object.
+     * @param {number} y - The y-coordinate of the throwable object.
+     * @param {string} [type='normal'] - The type of the throwable object ('normal' or 'poison').
+     */
     constructor(x, y, type = 'normal') {
         super().loadImg("/public/img/1.Sharkie/4.Attack/Bubble trap/Bubble.png");
         this.x = x;
@@ -14,6 +22,10 @@ class ThrowableObject extends MoveableObject {
         this.hitboxHeight = 50;
     }
 
+    /**
+     * Loads the appropriate image for the throwable object based on its type.
+     * If the object is "poison", it loads the poisoned bubble image. Otherwise, it loads the normal bubble image.
+     */
     loadImageForType() {
         if (this.type === "poison") {
             this.loadImg("/public/img/1.Sharkie/4.Attack/Bubble trap/Poisoned Bubble (for whale).png");
@@ -22,6 +34,12 @@ class ThrowableObject extends MoveableObject {
         }
     }
 
+    /**
+     * Throws the object in a specified direction. The object's vertical speed is set, and gravity is applied.
+     * It also moves horizontally in the specified direction.
+     * 
+     * @param {number} direction - The direction in which to throw the object (1 for right, -1 for left).
+     */
     throw(direction) {
         this.speedY = 20;
         this.applyGravity();
