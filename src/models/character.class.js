f3473efc61ee5b48861e695ddf67e9de2d577c26
M1roel <ppfautsch@yahoo.de>
2024-11-30 21:32:23 +0100

    JSDocs comments for char class

--- src/models/character.class.js
+++ src/models/character.class.js
@@ -51,6 +51,10 @@ class Character extends MoveableObject {
     });
   }
 
+  /**
+   * Animates the character's actions by updating different movement and state animations.
+   * Calls methods to animate movement, idle state, slap action, shot action, and camera update.
+   */
   animate() {
     this.animateMovement();
     this.animateLongIdle();
@@ -60,6 +64,10 @@ class Character extends MoveableObject {
     this.moveCharacter();
   }
 
+  /**
+ * Animates the character's movement based on the current state (e.g., idle, swimming, dead).
+ * Updates the animation at a rate of 5 frames per second.
+ */
   animateMovement() {
     setInterval(() => {
       if (this.isDead()) {
@@ -76,26 +84,35 @@ class Character extends MoveableObject {
     }, 1000 / 5);
   }
 
+  /**
+ * Animates the character's long idle state.
+ * The animation triggers when the character is idle for a certain period.
+ * Checks if the character is dead or if the idle state should be reset.
+ * Updates the idle timer and starts the long idle animation when conditions are met.
+ */
   animateLongIdle() {
     setInterval(() => {
       if (this.isDead()) {
         return;
       }
-
       if (this.checkForIdleReset()) {
         return;
       }
-
       this.idleTimer += 1000 / 5;
-
       if (this.idleTimer >= this.idleLimit && !this.longIdleInProgress) {
         this.startLongIdle();
       }
-
       this.longIdleAnimation();
     }, 1000 / 5);
   }
 
+  /**
+ * Checks if any key is pressed that should reset the long idle state.
+ * If any of the specified keys are pressed (e.g., movement keys, space, or action keys),
+ * it resets the long idle timer and returns true.
+ * 
+ * @returns {boolean} True if an idle reset is triggered, otherwise false.
+ */
   checkForIdleReset() {
     if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT || this.world.keyboard.SPACE || this.world.keyboard.UP || this.world.keyboard.DOWN || this.world.keyboard.E || this.world.keyboard.Q) {
       this.resetLongIdle();
@@ -104,16 +121,29 @@ class Character extends MoveableObject {
     return false;
   }
 
+  /**
+ * Starts the long idle animation by setting the appropriate flags.
+ * Sets the `longIdleInProgress` flag to true and resets the current animation image.
+ */
   startLongIdle() {
     this.longIdleInProgress = true;
     this.currentImage = 0;
   }
 
+  /**
+ * Resets the long idle state.
+ * Clears the idle timer and stops the long idle animation by setting the appropriate flags.
+ */
   resetLongIdle() {
     this.idleTimer = 0;
     this.longIdleInProgress = false;
   }
 
+  /**
+ * Updates the long idle animation by cycling through a set of images.
+ * If the long idle animation is in progress, it updates the current animation frame.
+ * After reaching the initial set of frames, it loops back through a subset of the frames.
+ */
   longIdleAnimation() {
     if (this.longIdleInProgress) {
       if (this.currentImage < 9) {
@@ -129,6 +159,12 @@ class Character extends MoveableObject {
     }
   }
 
+  /**
+ * Animates the slap action for the character.
+ * Calls methods to start the slap animation, update the slap animation frame,
+ * and check for collisions related to the slap.
+ * Runs at a rate of 10 frames per second.
+ */
   animateSlap() {
     setInterval(() => {
       if (this.isDead()) {
@@ -140,6 +176,11 @@ class Character extends MoveableObject {
     }, 1000 / 10);
   }
 
+  /**
+ * Initiates the slap action when the SPACE key is pressed.
+ * Resets the current animation frame and sets the `slapInProgress` flag to true 
+ * if a slap is not already in progress.
+ */
   startSlap() {
     if (this.world.keyboard.SPACE && !this.slapInProgress) {
       this.slapInProgress = true;
@@ -147,6 +188,11 @@ class Character extends MoveableObject {
     }
   }
 
+  /**
+ * Updates the slap animation by cycling through the slap images.
+ * If the slap action is in progress, it advances through the frames, plays the slap sound,
+ * and resets the animation once it reaches the end of the image sequence.
+ */
   slapAnimation() {
     if (this.slapInProgress) {
       if (this.currentImage < this.IMAGES_SLAP.length) {
@@ -161,6 +207,11 @@ class Character extends MoveableObject {
     }
   }
 
+  /**
+ * Animates the shot action by managing the shot process, bubble animation, 
+ * and checking for bubble collisions.
+ * Runs at a rate of 10 frames per second.
+ */
   animateShot() {
     setInterval(() => {
       if (!this.isDead()) {
@@ -171,66 +222,154 @@ class Character extends MoveableObject {
     }, 1000 / 10);
   }
 
+  /**
+ * Initiates the shot action based on the key pressed (E or Q).
+ * Starts either a normal shot or a poison shot, depending on the keyboard input,
+ * if no shot is already in progress and the shot has not been pressed yet.
+ */
   startShot() {
-    if (!this.shotPressed) {
-      if (this.world.keyboard.E && !this.shotInProgress) {
-        this.shotPressed = true;
-        this.shotInProgress = true;
-        this.currentImage = 0;
-        this.createBubble("normal");
-        this.bubble_sound.play();
-      } else if (this.world.keyboard.Q && !this.shotInProgress) {
-        this.shotPressed = true;
-        this.shotInProgress = true;
-        this.currentImage = 0;
-        if (this.world.bottleStatusbar.bottleCollect === 0) {
-          this.loadAnimation("IMAGES_WITHOUT_BUBBLE");
-        } else {
-          this.createBubble("poison");
-          this.bubble_sound.play();
-          this.world.bottleStatusbar.decreaseBottleCount();
-        }
+    if (!this.shotPressed && !this.shotInProgress) {
+      if (this.world.keyboard.E) {
+        this.startNormalShot();
+      } else if (this.world.keyboard.Q) {
+        this.startPoisonShot();
       }
     }
   }
 
+  /**
+   * Starts a normal shot.
+   */
+  startNormalShot() {
+    this.shotPressed = true;
+    this.shotInProgress = true;
+    this.currentImage = 0;
+    this.createBubble("normal");
+    this.bubble_sound.play();
+  }
+
+  /**
+   * Starts a poison shot.
+   */
+  startPoisonShot() {
+    this.shotPressed = true;
+    this.shotInProgress = true;
+    this.currentImage = 0;
+    if (this.world.bottleStatusbar.bottleCollect === 0) {
+      this.loadAnimation("IMAGES_WITHOUT_BUBBLE");
+    } else {
+      this.createBubble("poison");
+      this.bubble_sound.play();
+      this.world.bottleStatusbar.decreaseBottleCount();
+    }
+  }
+
+  /**
+ * Handles the animation and progress of the shot bubble.
+ * Updates the shot bubble image based on the current state.
+ * If the shot is completed, it resets the shot state.
+ */
   shotBubble() {
     if (this.shotInProgress) {
-      let imagesToUse;
-
-      if (this.world && this.world.keyboard && this.world.keyboard.E) {
-        imagesToUse = this.IMAGES_WITH_BUBBLE;
-      } else if (this.world && this.world.keyboard && this.world.keyboard.Q) {
-        imagesToUse = this.world.bottleStatusbar && this.world.bottleStatusbar.bottleCollect > 0 ? this.IMAGES_WITH_PBUBBLE : this.IMAGES_WITHOUT_BUBBLE;
-      }
+      const imagesToUse = this.getImagesToUse();
 
       if (imagesToUse && this.currentImage < imagesToUse.length) {
-        let path = imagesToUse[this.currentImage];
-        this.img = this.imageCache[path];
-        this.currentImage++;
+        this.updateShotImage(imagesToUse);
       } else {
-        this.shotInProgress = false;
-        this.shotPressed = false;
-        this.currentImage = 0;
+        this.resetShot();
       }
     }
   }
 
+  /**
+   * Determines the images to use based on the keyboard input.
+   * @returns {Array} List of image paths.
+   */
+  getImagesToUse() {
+    if (this.world.keyboard.E) {
+      return this.IMAGES_WITH_BUBBLE;
+    } else if (this.world.keyboard.Q) {
+      return this.getBottleImages();
+    }
+    return null;
+  }
+
+  /**
+   * Returns the correct bubble images based on the bottle status.
+   * @returns {Array} List of image paths for bubbles.
+   */
+  getBottleImages() {
+    return this.world.bottleStatusbar.bottleCollect > 0 ? this.IMAGES_WITH_PBUBBLE : this.IMAGES_WITHOUT_BUBBLE;
+  }
+
+  /**
+   * Updates the current shot image.
+   * @param {Array} imagesToUse List of image paths.
+   */
+  updateShotImage(imagesToUse) {
+    let path = imagesToUse[this.currentImage];
+    this.img = this.imageCache[path];
+    this.currentImage++;
+  }
+
+  /**
+   * Resets the shot progress.
+   */
+  resetShot() {
+    this.shotInProgress = false;
+    this.shotPressed = false;
+    this.currentImage = 0;
+  }
+
+  /**
+ * Creates a new bubble object and adds it to the world's throwable objects.
+ * The bubble's position is calculated based on the character's current state,
+ * and its direction is determined by the character's facing direction.
+ * 
+ * @param {string} type - The type of the bubble to create (e.g., "normal" or "poison").
+ */
   createBubble(type) {
-    const direction = this.otherDirection ? -1 : 1;
-    const bubbleX = this.otherDirection ? this.x + 20 * direction : this.x + 150 * direction;
+    const bubbleX = this.calculateBubbleX();
     const bubbleY = this.y + 50;
     const bubble = new ThrowableObject(bubbleX, bubbleY, type);
-    bubble.throw(direction);
+    bubble.throw(this.getThrowDirection());
     this.world.throwableObjects.push(bubble);
   }
 
+  /**
+   * Calculates the X position for the bubble based on direction.
+   * @returns {number} The X position for the bubble.
+   */
+  calculateBubbleX() {
+    const direction = this.getThrowDirection();
+    return this.otherDirection ? this.x + 20 * direction : this.x + 150 * direction;
+  }
+
+  /**
+   * Returns the direction for throwing the bubble.
+   * @returns {number} The throw direction.
+   */
+  getThrowDirection() {
+    return this.otherDirection ? -1 : 1;
+  }
+
+  /**
+ * Updates the camera's position to follow the character's movement.
+ * The camera's X position is adjusted based on the character's X position, 
+ * with an offset of 100 units to keep the character in view.
+ * Runs at 60 frames per second.
+ */
   updateCamera() {
     setInterval(() => {
       this.world.camera_x = -this.x + 100;
     }, 1000 / 60);
   }
 
+  /**
+ * Handles the character's movement by updating the position based on user input.
+ * Moves the character horizontally and vertically if the character is not dead.
+ * Runs at 60 frames per second.
+ */
   moveCharacter() {
     setInterval(() => {
       if (this.isDead()) {
@@ -241,6 +380,13 @@ class Character extends MoveableObject {
     }, 1000 / 60);
   }
 
+  /**
+ * Handles the character's horizontal movement based on keyboard input.
+ * Moves the character to the right if the RIGHT key is pressed, as long as the 
+ * character hasn't reached the end of the level. Moves the character to the left 
+ * if the LEFT key is pressed, as long as the character isn't at the left edge.
+ * Also updates the character's facing direction.
+ */
   handleHorizontalMovement() {
     if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
       this.x += 3;
@@ -253,16 +399,22 @@ class Character extends MoveableObject {
     }
   }
 
+  /**
+ * Handles the character's vertical movement based on keyboard input.
+ * Moves the character up if the UP key is pressed, as long as the character hasn't
+ * reached the minimum Y position. Moves the character down if the DOWN key is pressed,
+ * as long as the character hasn't reached the maximum Y position.
+ */
   handleVerticalMovement() {
     const minY = -90;
     const maxY = 320;
 
     if (this.world.keyboard.UP && this.y > minY) {
-        this.y -= 3;
+      this.y -= 3;
     }
 
     if (this.world.keyboard.DOWN && this.y < maxY) {
-        this.y += 3;
+      this.y += 3;
     }
   }
 }
